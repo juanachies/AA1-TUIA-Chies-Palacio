d33299a0f60a911fd3b8ecd91938c2ea4d448a9d
juana <jchiesdoumecq@gmail.com>
2025-11-29 23:05:37 -0300

    Finalizacion entrega 3

--- docker/inferencia.py
+++ docker/inferencia.py
@@ -6,8 +6,6 @@ import os
 
 from cleaner import DataCleaner
 
-# Desactivar logs de tensorflow
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 def cargar_artefactos():     
     model = tf.keras.models.load_model('red_neuronal.h5')
@@ -20,52 +18,49 @@ def cargar_artefactos():
 
 
 def preprocesar_datos(df, scaler, cleaner, model_columns, mapa_regiones):
-    # Agregado de Region
+    df = df.copy()
+
+    # Convertir date
+    if 'Date' in df.columns:
+        df['Date'] = pd.to_datetime(df['Date'])
+
+    # Creacion de region
     if 'Location' in df.columns:
         df['Region'] = df['Location'].map(mapa_regiones).fillna(0)
-    
-    # Codificación de variables categóricas
-    if 'RainToday' in df.columns:
-        df['RainToday'] = df['RainToday'].map({'No': 0, 'Yes': 1})
 
-    drop = ['Location']
-    df = df.drop(columns=[c for c in drop if c in df.columns], errors='ignore')
+    # Imputacion
+    df_clear = cleaner.transform(df)
 
-    df_processed = pd.get_dummies(df, drop_first=True)
+    # Codificación
+    df_processed = pd.get_dummies(df_clear, drop_first=True)
 
-    # Alinear columnas con las del entrenamiento
+    # Alinear columnas del modelo
     df_processed = df_processed.reindex(columns=model_columns, fill_value=0)
 
-    # Imputación
-    data_clear = cleaner.transform(df_processed)
+    # Escalado
+    data_scaled = scaler.transform(df_processed)
 
-    data_scaled = scaler.transform(data_clear)
-    
     return data_scaled
 
 
 def predict(df):
     model, scaler, cleaner, model_columns, mapa_regiones = cargar_artefactos()
 
-    df_procesado = preprocesar_datos(df, scaler, cleaner, model_columns, mapa_regiones)
+    X = preprocesar_datos(df, scaler, cleaner, model_columns, mapa_regiones)
 
-    predicciones = model.predict(df_procesado, verbose=0)
-    y_pred = np.argmax(predicciones, axis=1)
+    preds = model.predict(X, verbose=0)
 
-    for i, clase in enumerate(y_pred):
-        probabilidad = predicciones[i][clase] * 100
-        resultado = "LLUEVE" if clase == 1 else "NO LLUEVE"
-        print(f"Fila {i} -> Predicción: {resultado} mañana | Probabilidad: {probabilidad:.2f}%")
+    # Probabilidad de lluvia
+    p_lluvia = preds[:, 1]
 
-    # y_pred, y_proba = model.predict(df_procesado)
+    # Predicción final
+    y_pred = (p_lluvia >= 0.5).astype(int)
 
-    # resultado = "LLUEVE" if predicciones == 1 else "NO LLUEVE"
-    # probabilidad = y_proba * 100
+    resultado = "llueve" if y_pred == 1 else "no llueve"
 
-    # print(f"Predicción: {resultado} mañana")
-    # print(f"Probabilidad: {probabilidad:.2f}%")
+    print(f"Probabilidad de lluvia: {p_lluvia[0]*100:.2f}% | Predicción: {resultado} mañana ")
 
-    return y_pred, predicciones
+    return y_pred, p_lluvia
 
 
 def main(ejemplo):
